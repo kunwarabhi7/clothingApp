34eec1b9bf4abe1abd46b645a1371883a5f86087
kunwarabhi7 <104254107+kunwarabhi7@users.noreply.github.com>
2022-07-30 15:46:48 +0530

    Add files via upload
    Created Nav Links

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Home from "./routes/home/home.component";
+import { Routes, Route } from 'react-router-dom'
+import Navigation from "./routes/navigation/navigation.component";
+
+
+
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App;
